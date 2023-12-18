a0e5866f5c239a773db06dd76e189693b5a9db64
faquinoortiz <144743802+faquinoortiz@users.noreply.github.com>
2023-12-18 15:11:07 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-//imports
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Courses from './Courses';
 import CreateCourse from './CreateCourse';
 import UpdateCourse from './UpdateCourse';
@@ -8,36 +7,22 @@ import CourseDetail from './CourseDetail';
 import UserSignIn from './UserSignIn';
 import UserSignUp from './UserSignUp';
 import UserSignOut from './UserSignOut';
+import Header from './Header'; 
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <NavLink exact to="/">
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/courses/create">Create Course</NavLink>
-            </li>
-            
-          </ul>
-        </nav>
-<Router>
-        <Switch>
-          <Route path="/courses/create" component={CreateCourse} />
-          <Route path="/courses/:id/update" component={UpdateCourse} />
-          <Route path="/courses/:id" component={CourseDetail} />
-          <Route path="/signin" component={UserSignIn} />
-          <Route path="/signup" component={UserSignUp} />
-          <Route path="/signout" component={UserSignOut} />
-          <Route path="/" component={Courses} />
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/courses/create" element={<CreateCourse />} />
+        <Route path="/courses/:id/update" element={<UpdateCourse />} />
+        <Route path="/courses/:id" element={<CourseDetail />} />
+        <Route path="/signin" element={<UserSignIn />} />
+        <Route path="/signup" element={<UserSignUp />} />
+        <Route path="/signout" element={<UserSignOut />} />
+        <Route path="/" element={<Courses />} />
+      </Routes>
+    </div>
   );
 };
 
